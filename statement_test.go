15650b19080b541fea8f533f65852f0e9e9a9d5b
Viacheslav Poturaev <vearutop@gmail.com>
2025-08-07 19:11:19 +0200

    Add SplitStatements (#25)

---
+++ statement_test.go
@@ -0,0 +1,36 @@
+package sqluct_test
+
+import (
+	"testing"
+
+	"github.com/bool64/sqluct"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestSplitStatements(t *testing.T) {
+	s := `SELECT "ol'o""lo","",'';
+	-- next sta;tement
+	SELECT * FROM refers order by ts desc limit 15;
+	-- next statement
+	SELECT * FROM visitor WHERE hash=8361038239347337526;
+SELECT /* 1;2;3 */ 'aaa' 
+`
+
+	res := sqluct.SplitStatements(s)
+	assert.Equal(t, []string{
+		`SELECT "ol'o""lo","",''`,
+		"-- next sta;tement\n\tSELECT * FROM refers order by ts desc limit 15",
+		"-- next statement\n\tSELECT * FROM visitor WHERE hash=8361038239347337526",
+		"SELECT /* 1;2;3 */ 'aaa'",
+	}, res)
+}
+
+func TestSplitStatements2(t *testing.T) {
+	s := `";";';'`
+	res := sqluct.SplitStatements(s)
+
+	assert.Equal(t, []string{
+		`";"`,
+		`';'`,
+	}, res)
+}
